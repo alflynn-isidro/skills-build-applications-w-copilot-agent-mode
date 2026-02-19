067fd05cc6fae2fb62a79d3208a77bd668a5e23d
alflynn-isidro <alflynn.i.s.isidro@accenture.com>
2026-02-19 12:39:06 +0000

    commit 1234567890abcdef1234567890abcdef12345678 Author: Your Name <alflynn-isidro> Date:   Wed Jun 26 12:34:56 2024 +0000
        Use custom user model and update URLs for authentication

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -28,8 +28,18 @@ SECRET_KEY = 'django-insecure-3qn-m-z4(1za)$si*tivk95#@6$r*#rwsem@4ax%n5w7#ovou-
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+
+# Allow codespace URL and localhost
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    ALLOWED_HOSTS = [
+        f"{codespace_name}-8000.app.github.dev",
+        'localhost',
+        '127.0.0.1',
+    ]
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
